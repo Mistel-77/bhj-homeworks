4f609ab2362573b7882a7257826370f1c9e947e1
Yulia Klyueva <liya-n@list.ru>
2020-03-11 22:02:43 +0300

    second task

---
+++ element-search/menu/task.js
@@ -0,0 +1,19 @@
+const menuLink = Array.from(document.querySelectorAll(".menu_main .menu__link"));
+
+for (let i of menuLink) {
+    i.onclick = function() {
+
+        if (i.parentElement.querySelector(".menu_sub")) {
+            let oldClass = i.parentElement.querySelector(".menu_sub").className;
+
+            if (document.querySelector(".menu_active")) {
+                document.querySelector(".menu_active").className = oldClass;
+            }
+
+            i.parentElement.querySelector(".menu_sub").className = oldClass + " menu_active";
+
+            return false;
+
+       } 
+    }
+}
