c995bad7a4d476822de6e1a8f9afd72405356681
Yulia Klyueva <liya-n@list.ru>
2020-03-21 14:28:04 +0300

    task 2 add

---
+++ document-structure/todo/task.js
@@ -0,0 +1,32 @@
+const button = document.getElementById('tasks__add');
+const input = document.getElementById('task__input');
+const tasks_list = document.getElementById('tasks__list');
+
+function toDo() {
+    console.log('4');
+    let html = `<div class="task">
+         <div class="task__title">
+              ${input.value}
+            </div>
+            <a href="#" class="task__remove">&times;</a>
+        </div>`;
+    tasks_list.insertAdjacentHTML('beforeEnd', html);
+    tasks_list.lastElementChild.querySelector('.task__remove').addEventListener('click', (event) => {
+        event.target.closest('.task').remove();
+    });
+    input.value = '';
+}
+
+input.addEventListener('keydown', (event) => {
+    if(event.key == "Enter" && input.value != '') {
+        event.preventDefault();
+        toDo();
+    }  
+});
+
+button.addEventListener('click', (event) => {
+    if(input.value != '') {
+        event.preventDefault();
+        toDo();
+    }  
+});
